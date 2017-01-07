4f60629cc6a78c0a23e12c29a8b11b88b51619b7
Kanjyosen <trr95goo@gmail.com>
2017-01-07 18:46:33 +0900

    Zippingging

--- src/main/lib.java
+++ src/main/lib.java
@@ -1,2 +1,12 @@
 package main;
-public class NewWindow extends JFrame {
+/*public class NewWindow extends JFrame {
+	JPanel p = new JPanel();
+	
+}*/
+public class lib {
+	public class zipping {
+		String Zipname = new String();
+		String Filename = new String();
+		final File file = new File()
+	}
+	}

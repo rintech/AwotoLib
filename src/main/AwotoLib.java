56576b5c32798d8860aab4b92cd23f74a605de22
Kanjyosen <trr95goo@gmail.com>
2017-03-23 23:24:26 +0900

    もはやDialog.javaがメインのファイルと化している

--- src/main/AwotoLib.java
+++ src/main/AwotoLib.java
@@ -1,5 +1,5 @@
 package main;
-
+/*
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,56 +11,10 @@ import java.util.zip.ZipOutputStream;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
-	public void Dialog(String text) {
-		JOptionPane.showMessageDialog(this, text);
-	}
 	
-	public void Question(String Message, String Title) {
-		JFrame frame = new JFrame();
-		int answer = JOptionPane.showConfirmDialog(frame, Title, Message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-		if (answer == JOptionPane.YES_OPTION){
-			correct = "yes";
-		}
-		if (answer == JOptionPane.NO_OPTION) {
-			correct = "no";
-		}
-	}
-	public void TxtfileWrite(String writtentext) {
-		JFileChooser filechooser = new JFileChooser();
-		int selected = filechooser.showOpenDialog(this);
-		if (selected == JFileChooser.APPROVE_OPTION){
-				File file = filechooser.getSelectedFile();
-					try{
-						if (file.exists()){
-							question("同名のファイルが存在します", "同じ名前のファイル" + file + "が存在します。上書きしますか?");
-								if(correct == "yes") {
-									file.createNewFile();
-									FileWriter filewriter = new FileWriter(file);
-									filewriter.write(writtentext);
-									filewriter.close();
-									Dialog("書き込みが完了しました"); 
-								}else{
-									Dialog("書き込みを中止しました");
-								}
-						}else{
-							file.createNewFile();
-							FileWriter filewriter = new FileWriter(file);
-							filewriter.write(writtentext);
-							filewriter.close();
-							Dialog("書き込みが完了しました"); 
-						}
-						}catch(IOException e){
-						Dialog("エラー:" + e);
-					}
-					
-			}else if (selected == JFileChooser.CANCEL_OPTION){
-				Dialog("キャンセルされました");
-			}else if (selected == JFileChooser.ERROR_OPTION){
-				Dialog("エラー又は取消しがありました");
-			}
-	}
-	}
+	
+	}*/
+	
 	//public static void main(String[] args) {
 /*		try {
 			File file = new File("test.zip");
--- src/main/Dialog.java
+++ src/main/Dialog.java
@@ -4,18 +4,19 @@ import java.awt.Desktop;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class Dialog {
-	public Dialog(String text) {
-		JLabel frame = new JLabel("frame");
-		JOptionPane.showMessageDialog(frame, text);
-	}
+	
 
 	public void Clipboard(String select) { // 構文コピーメソッド
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -31,5 +32,54 @@ public class Dialog {
 		catch (IOException String) {
 		} catch (URISyntaxException e) {
 		}
+		}
+	
+	public void Question(String Message, String Title, String correct) {
+		JFrame frame = new JFrame();
+		int answer = JOptionPane.showConfirmDialog(frame, Title, Message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+		if (answer == JOptionPane.YES_OPTION){
+			correct = "yes";
+		}
+		if (answer == JOptionPane.NO_OPTION) {
+			correct = "no";
+		}
 	}
-}
+	public void TxtfileWrite(String writtentext, String correct) {
+		JFileChooser filechooser = new JFileChooser();
+		int selected = filechooser.showOpenDialog(this);
+		if (selected == JFileChooser.APPROVE_OPTION){
+				File file = filechooser.getSelectedFile();
+					try{
+						if (file.exists()){
+							Question("同名のファイルが存在します", "同じ名前のファイル" + file + "が存在します。上書きしますか?", correct);
+								if(correct == "yes") {
+									file.createNewFile();
+									FileWriter filewriter = new FileWriter(file);
+									filewriter.write(writtentext);
+									filewriter.close();
+									Dialog("書き込みが完了しました"); 
+								}else{
+									Dialog("書き込みを中止しました");
+								}
+						}else{
+							file.createNewFile();
+							FileWriter filewriter = new FileWriter(file);
+							filewriter.write(writtentext);
+							filewriter.close();
+							Dialog("書き込みが完了しました"); 
+						}
+						}catch(IOException e){
+						Dialog("エラー:" + e);
+					}
+					
+			}else if (selected == JFileChooser.CANCEL_OPTION){
+				Dialog("キャンセルされました");
+			}else if (selected == JFileChooser.ERROR_OPTION){
+				Dialog("エラー又は取消しがありました");
+			}
+	}
+	public Dialog(String text) {
+		JLabel frame = new JLabel("frame");
+		JOptionPane.showMessageDialog(frame, text);
+	}
+	}

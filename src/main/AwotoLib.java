93df8a789ca14641a87620980f6fb3d63079b59d
Kanjyosen <trr95goo@gmail.com>
2017-03-23 20:00:25 +0900

    no message

--- src/main/AwotoLib.java
+++ src/main/AwotoLib.java
@@ -3,24 +3,77 @@ package main;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class AwotoLib {
-	public static void main(String[] args) {
-		try {
+	String correct = new String();
+	public void Dialog(String text) {
+		
+		JOptionPane.showMessageDialog(this, text);
+	}
+	public void question(String Message, String Title) {
+		JFrame frame = new JFrame();
+		int answer = JOptionPane.showConfirmDialog(frame, Title, Message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+		if (answer == JOptionPane.YES_OPTION){
+			correct = "yes";
+		}
+		if (answer == JOptionPane.NO_OPTION) {
+			correct = "no";
+		}
+	}
+	public void TxtfileWrite(String writtentext) {
+		JFileChooser filechooser = new JFileChooser();
+		int selected = filechooser.showOpenDialog(this);
+		if (selected == JFileChooser.APPROVE_OPTION){
+				File file = filechooser.getSelectedFile();
+					try{
+						if (file.exists()){
+							question("同名のファイルが存在します", "同じ名前のファイル" + file + "が存在します。上書きしますか?");
+								if(correct == "yes") {
+									file.createNewFile();
+									FileWriter filewriter = new FileWriter(file);
+									filewriter.write(writtentext);
+									filewriter.close();
+									Dialog("書き込みが完了しました"); 
+								}else{
+									Dialog("書き込みを中止しました");
+								}
+						}else{
+							file.createNewFile();
+							FileWriter filewriter = new FileWriter(file);
+							filewriter.write(writtentext);
+							filewriter.close();
+							Dialog("書き込みが完了しました"); 
+						}
+						}catch(IOException e){
+						Dialog("エラー:" + e);
+					}
+					
+			}else if (selected == JFileChooser.CANCEL_OPTION){
+				Dialog("キャンセルされました");
+			}else if (selected == JFileChooser.ERROR_OPTION){
+				Dialog("エラー又は取消しがありました");
+			}
+	//public static void main(String[] args) {
+/*		try {
 			File file = new File("test.zip");
 			zipping(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	public static void zipping(/*File dir, */File zipfile) throws IOException {
+	public static void zipping(/*File dir, *//*File zipfile) throws IOException {
 		ZipOutputStream zipOutStream = new ZipOutputStream(
 			new BufferedOutputStream(new FileOutputStream(zipfile)));
 		{
-			/* ディレクトリの作成 */
+			/* ディレクトリの作成 *//*
 			ZipEntry entry = new ZipEntry("ABC/");
 			entry.setMethod(ZipEntry.STORED);
 			entry.setSize(0);
@@ -31,7 +84,7 @@ public class AwotoLib {
 		{
 			ZipEntry entry = new ZipEntry("ABC/ABC.txt");
 			zipOutStream.putNextEntry(entry);
-			/* ファイルデータの書き込み */
+			/* ファイルデータの書き込み *//*
 			zipOutStream.write(
 				new String("あいうえお").getBytes());
 			zipOutStream.closeEntry();
@@ -52,5 +105,7 @@ public class AwotoLib {
 		}
 		zipOutStream.finish();
 		zipOutStream.close();
-	}
+	}*/
+	//}
+	
 }
